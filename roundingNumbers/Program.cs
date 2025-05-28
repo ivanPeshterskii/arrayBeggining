968fc5d66d21c872a1e74d03bc579d5b1d7a725a
ivanPeshterskii <peshterskiivan@gmail.com>
2025-05-28 22:32:54 +0300

    Add files via upload

---
+++ roundingNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq; 
+
+double[] numbers = Console.ReadLine()
+    .Split()
+    .Select(double.Parse)
+    .ToArray();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int rounded = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+    Console.WriteLine($"{numbers[i]} => {rounded}");
+}
