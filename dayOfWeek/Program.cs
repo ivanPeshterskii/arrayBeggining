acec857d09174168f26d155890075afb358f553b
ivanPeshterskii <peshterskiivan@gmail.com>
2025-05-28 22:31:09 +0300

    Add files via upload

---
+++ dayOfWeek/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+string[] day = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine(day[number - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
