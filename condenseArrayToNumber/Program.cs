da733731d3d6b15ab3c7a35774d7faacec1d7e1a
ivanPeshterskii <peshterskiivan@gmail.com>
2025-05-29 20:54:08 +0300

    Add files via upload

---
+++ condenseArrayToNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the array from the console
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Keep condensing until only one element is left
+        while (numbers.Length > 1)
+        {
+            int[] condensed = new int[numbers.Length - 1];
+
+            for (int i = 0; i < condensed.Length; i++)
+            {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+
+            numbers = condensed;
+        }
+
+        // Output the final result
+        Console.WriteLine(numbers[0]);
+    }
+}
---
+++ equalArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+int[] arr1 = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] arr2 = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int sum = default;
+bool isIdentical = true;
+
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i] != arr2[i])
+    {
+        isIdentical = false;
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        break;
+    }
+    sum += arr1[i];
+
+    
+}
+if (isIdentical)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
+
