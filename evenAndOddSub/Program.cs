98da11522887e3a9047dc486316197e129841019
ivanPeshterskii <peshterskiivan@gmail.com>
2025-05-29 20:52:53 +0300

    Add files via upload

---
+++ evenAndOddSub/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int evenSum = default;
+int oddSum = default;
+int result = default;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        evenSum += numbers[i]; 
+    }
+    else
+    {
+        oddSum += numbers[i]; 
+    }
+    result = evenSum - oddSum; 
+}
+
+Console.WriteLine(result);
---
+++ sumEvenNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int sum = default;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    
+    if (numbers[i] % 2 == 0)
+    {
+        sum += numbers[i]; 
+    }
+}
+
+Console.WriteLine(sum);
